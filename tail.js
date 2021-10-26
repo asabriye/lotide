6ec24cf2377b6fedc717c23aceb4488b1b920694
asabriye <abdullahisabriye@gmail.com>
2021-10-26 19:11:49 +0000

    tail.js code

---
+++ tail.js
@@ -0,0 +1,24 @@
+const tail = function(array) {
+  let tail = array.slice(1);
+  return tail;
+
+};
+tail(["Hello", "Lighthouse", "Labs"]);
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+
+  }
+};
+
+module.exports = tail;
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+assertEqual(tail(words).length, 2); // should PASS
