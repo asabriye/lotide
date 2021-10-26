4a167548f83c2bc3d6d8bec94de7af5705891122
asabriye <abdullahisabriye@gmail.com>
2021-10-26 19:07:46 +0000

    head.js code

---
+++ head.js
@@ -0,0 +1,13 @@
+const head = function(array) {
+
+  return array[0]
+
+};
+
+const assertEqual = require('./assertEqual');
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5); //PASS
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Helloo"); //FAIL
+
+
