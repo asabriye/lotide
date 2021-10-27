89a29cb6285f68df41c9f9f3da43b18552cc8f62
asabriye <abdullahisabriye@gmail.com>
2021-10-27 21:47:26 +0000

    assertEquals arrays

--- assertEqual.js
+++ assertEqual.js
@@ -1,18 +1,21 @@
 const assertEqual = function(actual, expected) {
-
   if (actual === expected) {
-    //console.log("✅✅✅ Assertion Passed: " + [actual] + " === " + [expected]);
-    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
-
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-
+    console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
   }
-
 };
 
-// // TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp"); //Fail
-// assertEqual(1, 1); //Pass
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+  for (let i = 0; 1 < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+    return false
+   }
+  }
+  return true;
+};
 
-module.exports = assertEqual;
+assertEqual(eqArrays([1, 2,], [1, 2, 3]));
