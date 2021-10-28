e3883c66a606dc55abed09af64bb45e124983d2a
asabriye <abdullahisabriye@gmail.com>
2021-10-28 19:35:54 +0000

    arrays equal code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,32 @@
-//assertEqual("6", "2");
-  //assertEqual(1, 1);
+const assertEqual = function(actual, expected) {
 
-  const countLetters = function (words) {
-    const obj = {} // this is where the letters are stored
-     for (let i =0; i < words.length; i ++) { // looping through the string
-      let letter = words[i]; // declaring letter as each index in the string 
-     if (obj[letter]) { // if statment to see if obj(letter)
-       obj[letter] ++ 
-     }
-     else {
-       obj[letter] = 1
-     }
-     }
-   return obj
-   } 
-   console.log (countLetters("lighthouse in the house"))
+    if (actual === expected) {
+       console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+     } else if (actual !== expected) {
+       console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+    }
+   };
+
+const eqArrays = function(arr1, arr2) {
+
+  let i = 0;
+  let answer = Boolean;
+
+  for (i = 0; i < arr1.length; i++) {
+
+    if (arr1.length === arr2.length) {
+      if (arr1[i] === arr2[i]) {
+        answer = true;
+      } else if (arr1[i] !== arr2[i]) {
+        answer = false;
+      }
+    } else {
+      answer = false
+    }
+  }
+  return answer;
+
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 9]), false)
+
