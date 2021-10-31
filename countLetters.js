3e3e7c59118bbbaafec942c26108625eb448c728
asabriye <abdullahisabriye@gmail.com>
2021-10-31 20:41:59 +0000

    corrected code

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,43 @@
 //assertEqual("6", "2");
   //assertEqual(1, 1);
 
+  const assertEqual = function(actual, expected) {
+
+    if (actual === expected) {
+       console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+     } else if (actual !== expected) {
+       console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+    }
+   };
+
+const eqArrays = function(arr1, arr2) {
+
+  let i = 0;
+  let answer = Boolean;
+
+  for (i = 0; i < arr1.length; i++) {
+
+    if (arr1.length === arr2.length) {
+      if (arr1[i] === arr2[i]) {
+        answer = true;
+      } else if (arr1[i] !== arr2[i]) {
+        answer = false;
+      }
+    } else {
+      answer = false
+    }
+  }
+  return answer;
+
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 9]), false)
+
   const countLetters = function (words) {
-    const obj = {} // this is where the letters are stored
-     for (let i =0; i < words.length; i ++) { // looping through the string
+    const obj = {} // store letters in here
+     for (let i =0; i < words.length; i ++) { // loop through the string
       let letter = words[i]; // declaring letter as each index in the string 
-     if (obj[letter]) { // if statment to see if obj(letter)
+     if (obj[letter]) { // statement to see if obj(letter) is true
        obj[letter] ++ 
      }
      else {
@@ -14,4 +46,4 @@
      }
    return obj
    } 
-   console.log (countLetters("lighthouse in the house"))
+   console.log (countLetters("Hello"))
